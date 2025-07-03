6a4a70e880035a4ff4616ed58edf50d5c1b58316
Kattya Roxana Desiderio Sanchez <kattya.desiderio@siigo.com>
2025-07-03 18:08:18 -0500

    Commit changes

---
+++ reto-typescript-main/src/exercises/19-construir-omit/index.ts
@@ -0,0 +1,47 @@
+/* _____________ Aqui va tu codigo _____________ */
+
+type MyOmit<T, K> = T extends object
+  ? {
+      [P in keyof T as P extends K ? never : P]: T[P] extends object
+        ? MyOmit<T[P], K>
+        : T[P];
+    }
+  : never;
+
+/* _____________ Casos de prueba  _____________ */
+
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
+  Expect<Equal<Expected3, MyOmit<Todo1, 'description' | 'completed'>>>,
+];
+
+
+type error = MyOmit<Todo, 'description' | 'invalid'>;
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Todo1 {
+  readonly title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+  completed: boolean;
+}
+
+interface Expected2 {
+  title: string;
+}
+
+interface Expected3 {
+  readonly title: string;
+}
---
+++ reto-typescript-main/src/exercises/20-construir-length/index.ts
@@ -0,0 +1,19 @@
+/* _____________ Aqui va tu codigo _____________ */
+
+
+type MyLength<S extends string, complement extends any[] = []> =
+  S extends `${infer _}${infer Rest}`
+    ? MyLength<Rest, [unknown, ...complement]>
+    : complement['length'];
+
+
+/* _____________ Casos de prueba  _____________ */
+
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<MyLength<'hola siigers'>, 12>>,
+  Expect<Equal<MyLength<''>, 0>>,
+  Expect<Equal<MyLength<'a'>, 1>>,
+  Expect<Equal<MyLength<'Siigo'>, 5>>,
+];
